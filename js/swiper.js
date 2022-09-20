b48cd51daeff321f937f4a950b486a455915bb50
Awer100 <awer100@gmail.com>
2022-09-20 01:08:08 -0600

    feat: slider

--- js/swiper.js
+++ js/swiper.js
@@ -10,12 +10,16 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: '.swiper-button-prev'
     },
     WatchOverflow: true,
-    spaceBetween: 30,
+    spaceBetween: 20,
 
   });
 
   var swiper = new Swiper(".mySwiper2", {
     slidesPerView: 4,
     spaceBetween: 13,
+    pagination: {
+        el: ".swiper-pagination",
+          type: "progressbar",
+      },
   });
 
