8b57428d55981e906dbf5b9c5970be350f965609
Awer100 <awer100@gmail.com>
2022-09-19 21:27:13 -0600

    feat: slider

---
+++ js/dist/swiper.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  slidesPerView: 2,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  WatchOverflow: true,
+  spaceBetween: 30
+});
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 4,
+  spaceBetween: 13
+});
