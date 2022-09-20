0b83f014e870f50a3d261a88d8709c7ac333d660
Awer100 <awer100@gmail.com>
2022-09-20 13:47:02 -0600

    freat: json

---
+++ js/json.js
@@ -0,0 +1,21 @@
+var services = {
+    10 : {'Nail services': {
+        1010 : 'male manicure',
+        1011 : 'gel extension',
+        1012 : 'manicure and extension',
+        1013 : 'coating nail'}},
+    11 : {'Make-up services' : {
+        1110 : 'evening make-up',
+        1111 : 'evening make-up'}},
+    12 : {'Hairdresser services' : {
+        1210 : 'men`s haircut',
+        1211 : 'women`s haircut',
+        1212 : 'hair coloring',
+        1213 : 'hair highlighting'}},
+    13 : {
+    'cosmetology services' : {
+        1310 : 'face cleaning',
+        1311 : 'depilation',
+        1312 : 'piling'}}}
+
+console.log(JSON.stringify(services, 2))
